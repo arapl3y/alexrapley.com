37a8559388d15f711ea622592e96cb192d586288
Alex Rapley <rapley3@gmail.com>
2016-08-13 15:00:59 +1000

    Another website revamp
    Changing the design of the website

--- js/main.js
+++ js/main.js
@@ -6,6 +6,67 @@ $(function() {
 });
 
 
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+  
+  // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+  
+  // scroll to top button fade
+  
+ $(function() {
+ 
+var offset = 250;
+ 
+var duration = 300;
+ 
+$(window).scroll(function() {
+ 
+if ($(this).scrollTop() > offset) {
+ 
+$('.scroll-top').fadeIn(duration);
+ 
+} else {
+ 
+$('.scroll-top').fadeOut(duration);
+ 
+}
+ 
+});
+ 
+$('.scroll-top').click(function(event) {
+ 
+event.preventDefault();
+ 
+$('html, body').animate({scrollTop: 0}, duration);
+ 
+return false;
+ 
+})
+ 
+});
+
+  })(jQuery); 
+
+
+
 
 
 /* Google Analytics */
