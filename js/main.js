4fb0fc18c23fad3d43c5abdf302207fe2ba9be7c
Alex Rapley <rapley3@gmail.com>
2016-09-22 14:03:44 +1000

    Complete Redesign
    Experimenting with some new design ideas, adding a journal+credits
    section. Also using codepen embedded links in projects section.

--- js/main.js
+++ js/main.js
@@ -1,80 +1,22 @@
+jQuery(document).ready(function($){
 
-/* Fade In */
-
-$(function() {
-  $('body').removeClass('fade-out');
+// open/close primary navigation
+$('.primary-nav-trigger').on('click', function() {
+	$('.menu-icon').toggleClass('is-clicked');
+	$('.header').toggleClass('menu-is-open');
+	
+		/* in firefox transitions break when parent overflow is changed, 
+		so we need to wait for the end of the trasition to give the body 
+		an overflow hidden */
+		
+		if( $('.primary-nav').hasClass('is-visible') ) {
+			$('.primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('body').removeClass('overflow-hidden');
+			});
+		} else {
+			$('.primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('body').addClass('overflow-hidden');
+			});
+		}
+	});
 });
-
-
-(function($) {
-    "use strict"; // Start of use strict
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-  
-  // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-  
-  // scroll to top button fade
-  
- $(function() {
- 
-var offset = 250;
- 
-var duration = 300;
- 
-$(window).scroll(function() {
- 
-if ($(this).scrollTop() > offset) {
- 
-$('.scroll-top').fadeIn(duration);
- 
-} else {
- 
-$('.scroll-top').fadeOut(duration);
- 
-}
- 
-});
- 
-$('.scroll-top').click(function(event) {
- 
-event.preventDefault();
- 
-$('html, body').animate({scrollTop: 0}, duration);
- 
-return false;
- 
-})
- 
-});
-
-  })(jQuery); 
-
-
-
-
-
-/* Google Analytics */
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-78494498-1', 'auto');
-    ga('send', 'pageview');
