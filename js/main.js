d38a22c73c70ba3fa923e5525bd91e2ade3c5467
Alex Rapley <rapley3@gmail.com>
2017-01-24 11:59:23 +1100

    Added new projects to portfolio section and new image thumbnails for older projects, removed modals with codepen embedding and replaced with direct links to hosted pages

--- js/main.js
+++ js/main.js
@@ -1,40 +1,54 @@
 $(document).ready(function() {
-    "use strict"; // Start of use strict
+  "use strict";
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
+  var app = {};
 
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
+  app.init = function() {
+    app.pageScroll();
+    app.scrollTopBtn();
+    app.buttonFade();
     // Closes the Responsive Menu on Menu Item Click
-
     $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-        $('.navbar-toggle:visible').click();
+      $('.navbar-toggle:visible').click();
     });
+  };
 
+  // Page scrolling - requires jQuery Easing plugin
+  app.pageScroll = function() {
+    $('.page-scroll a').bind('click', function(e) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+      }, 1250, 'easeInOutExpo');
+      e.preventDefault();
+    });
+  };
 
-
+	// Scroll to top of the page button
+  app.scrollTopBtn = function() {
     // scroll to top button fade
-    var offset = 250;
-    var duration = 300;
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-            $('.scroll-top').fadeIn(duration);
-        } else {
-            $('.scroll-top').fadeOut(duration);
-        }
-    });
-    $('.scroll-top').click(function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, duration);
-        return false;
-    });
+    app.offset = 250;
+    app.duration = 300;
 
+		$(window).scroll(function() {
+      if ($(this).scrollTop() > app.offset) {
+        $('.scroll-top').fadeIn(app.duration);
+      } else {
+        $('.scroll-top').fadeOut(app.duration);
+      }
+    });
+  };
+
+	// Scroll to top of the page button fade on scroll up
+	app.buttonFade = function() {
+  $('.scroll-top').click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, app.duration);
+    return false;
+  });
+};
+
+  app.init();
 });
